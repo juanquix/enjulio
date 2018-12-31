6d209239dc3857b81985c1b70333062aecc81940
juanqui2018 <juancarlos.pinto.avantica@gmail.com>
2018-12-31 11:38:29 -0400

    adding backend logic

---
+++ JulioRivero.Tesis.WebMVC/Controllers/IntoPreventionController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using JulioRivero.Tesis.Entities;
+using JulioRivero.Tesis.WebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JulioRivero.Tesis.WebMVC.Controllers
+{
+    public class IntoPreventionController : BaseController
+    {
+        // GET: IntoPrevention
+        public ActionResult Index()
+        {
+            var intoPreventions = Mapper.Map<IList<IntoPrevention>, IList<IntoPreventionViewModel>>(intoPreventionManager.GetAllIntoPreventions()).ToList();
+            return View(intoPreventions);
+        }
+
+        // GET: IntoPrevention/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = Mapper.Map<IntoPreventionViewModel>(intoPreventionManager.GetById(id));
+            return View(model);
+        }
+
+        // GET: IntoPrevention/Create
+        public ActionResult Create()
+        {
+            var model = new UserViewModel();
+            return View(model);
+        }
+
+        // POST: IntoPrevention/Create
+        [HttpPost]
+        public ActionResult Create(UserViewModel model)
+        {
+            try
+            {
+                var user = Mapper.Map<UserViewModel, User>(model);
+                userManager.StoreUser(user);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: IntoPrevention/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = Mapper.Map<User, UserViewModel>(userManager.GetById(id));
+            return View(model);
+        }
+
+        // POST: IntoPrevention/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, UserViewModel model)
+        {
+            try
+            {
+                var user = Mapper.Map<UserViewModel, User>(model);
+                userManager.StoreUser(user);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+
+        // GET: IntoPrevention/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var model = Mapper.Map<User, UserViewModel>(userManager.GetById(id));
+            return View(model);
+        }
+
+        // POST: IntoPrevention/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, UserViewModel model)
+        {
+            try
+            {
+                userManager.DeleteUser(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+    }
+}
---
+++ JulioRivero.Tesis.WebMVC/Controllers/PreventionController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using JulioRivero.Tesis.Entities;
+using JulioRivero.Tesis.WebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JulioRivero.Tesis.WebMVC.Controllers
+{
+    public class PreventionController : BaseController
+    {
+        // GET: Prevention
+        public ActionResult Index()
+        {
+            var preventions = Mapper.Map<IList<Prevention>, IList<PreventionViewModel>>(preventionManager.GetAllPreventions()).ToList();
+            return View(preventions);
+        }
+
+        // GET: Prevention/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = Mapper.Map<PreventionViewModel>(preventionManager.GetById(id));
+            return View(model);
+        }
+
+        // GET: Prevention/Create
+        public ActionResult Create()
+        {
+            var model = new PreventionViewModel();
+            return View(model);
+        }
+
+        // POST: Prevention/Create
+        [HttpPost]
+        public ActionResult Create(PreventionViewModel model)
+        {
+            try
+            {
+                var prevention = Mapper.Map<PreventionViewModel, Prevention>(model);
+                preventionManager.StorePrevention(prevention);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Prevention/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = Mapper.Map<Prevention, PreventionViewModel>(preventionManager.GetById(id));
+            return View(model);
+        }
+
+        // POST: Prevention/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, PreventionViewModel model)
+        {
+            try
+            {
+                var prevention = Mapper.Map<PreventionViewModel, Prevention>(model);
+                preventionManager.StorePrevention(prevention);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+
+        // GET: Prevention/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var model = Mapper.Map<Prevention, PreventionViewModel>(preventionManager.GetById(id));
+            return View(model);
+        }
+
+        // POST: Prevention/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, PreventionViewModel model)
+        {
+            try
+            {
+                preventionManager.DeletePrevention(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+    }
+}
